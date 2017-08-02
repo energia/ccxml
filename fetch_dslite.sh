0c7a8886109a251fbb60ba47a1498957c67e3149
Robert Wessels <robertinant@yahoo.com>
2017-08-02 12:53:09 -0700

    Change default install directory

--- fetch_dslite.sh
+++ fetch_dslite.sh
@@ -3,19 +3,17 @@
 #./fetch_dslite.sh <path to .ccxml files>
 unamestr=`uname`
 
+echo "Fetching TICloudAgent Installer"
+wget -O ticloudagent.run https://dev.ti.com/ticloudagent/getInstaller?os=linux
+
+echo "Installing TICloudagent"
+chmod u+x ticloudagent.run
+./ticloudagent.run --mode unattended --prefix $(pwd)
+
+echo -e "{\n\t\"userDataRoot\" : \"$(pwd)/TICloudAgent\"\n}" > TICloudAgent/config.json
+
 echo "Fetching DSLite"
-
-if [[ "$unamestr" == 'MINGW32_NT-6.2' ]]; then
-    dslite_dir="$LOCALAPPDATA/Texas Instruments/TICloudAgent/loaders/ccs_base"
-else
-    dslite_dir=~/.ti/TICloudAgent/loaders/ccs_base
-fi
-
-
-if [ "$#" -ne 1 ]; then
-    echo "Illegal number of parameters"
-    exit 0;
-fi
+dslite_dir=$(pwd)/TICloudAgent/loaders/ccs_base
 
 shopt -s nullglob
 
@@ -24,12 +22,13 @@ printf '%d ccxml files found\n' "${#files[@]}"
 
 for f in "${files[@]}"; do
   printf 'processing %s\n' "${f}"
-  $1/node $1/src/installer/cli.js --host http://vmtgccscloud00.toro.design.ti.com --offline 1 --target ${f}
+  $(pwd)/TICloudAgent/node $(pwd)/TICloudAgent/src/installer/cli.js --host https://dev.ti.com --offline 1 --target ${f}
 done
 
 echo "Packing DSLite"
 rm -r DSLite
 cp -r "${dslite_dir}" DSLite
+cp *.ccxml DSLite/
 
 DSLITE_VER="$(DSLite/DebugServer/bin/DSLite help | sed -n "s/^.*version \([0-9.]*\).*/\1/p")"
 echo "DSLite version is:" ${DSLITE_VER}
@@ -42,7 +41,7 @@ if [[ "$unamestr" == 'Darwin' ]]; then
 elif [[ "$unamestr" == 'Linux' ]]; then
     echo ${string} "Linux"
     eval "tar -cjf dslite-${DSLITE_VER}-i386-x86_64-pc-linux-gnu.tar.bz2 DSLite"
-elif [[ "$unamestr" == 'MINGW32_NT-6.2' ]]; then
+elif [[ "$unamestr" == '*MINGW32*' ]]; then
     echo ${string} "Windows"
     eval "zip -r dslite-${DSLITE_VER}-i686-mingw32.zip DSLite"
 fi
