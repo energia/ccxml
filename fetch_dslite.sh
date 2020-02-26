95e48a2cd3f3160e58ac512e20c0189348546951
Robert Wessels <robertinant@yahoo.com>
2020-02-26 12:13:41 -0800

    add copy of firmware.bin

--- fetch_dslite.sh
+++ fetch_dslite.sh
@@ -3,8 +3,8 @@
 #./fetch_dslite.sh <path to .ccxml files>
 unamestr=`uname`
 
-server=btcloudstaging.toro.design.ti.com
-#server=dev.ti.com
+#server=btcloudstaging.toro.design.ti.com
+server=dev.ti.com
 
 # trap ctrl-c and call ctrl_c()
 trap ctrl_c INT
@@ -18,7 +18,7 @@ echo "Fetching TICloudAgent Installer from server ${server}"
 
 echo "Installing TICloudagent"
 
-    unset https_proxy http_proxy
+#    unset https_proxy http_proxy
 if [[ "$unamestr" == 'Darwin' ]]; then
     wget --no-check-certificate -O ticloudagent.dmg https://${server}/ticloudagent/getInstaller?os=osx
     hdiutil attach -mountpoint ./ticloudagent_install ticloudagent.dmg
@@ -60,7 +60,9 @@ rm -r DSLite
 cp -r "${dslite_dir}" DSLite
 cp *.ccxml DSLite/
 
+cp firmware.bin DSLite/common/uscif/xds110/
 DSLITE_VER="$(DSLite/DebugServer/bin/DSLite help | sed -n "s/^.*version \([0-9.]*\).*/\1/p")"
+DSLITE_VER="${DSLITE_VER}-e1"
 echo "DSLite version is:" ${DSLITE_VER}
 
 string="Creating archive for "
